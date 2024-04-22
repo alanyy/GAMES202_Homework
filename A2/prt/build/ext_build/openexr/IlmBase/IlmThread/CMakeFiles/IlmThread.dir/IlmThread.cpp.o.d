b3c73068309171f0ae30232ffc848327adab2214
alanyy <alanyy1986@gmail.com>
2024-04-22 14:26:22 +0800

    finish homework 2

---
+++ A2/prt/build/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThread.cpp.o.d
@@ -0,0 +1,3 @@
+ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThread.cpp.o: \
+  /Users/alan/Src/Games202/A2/prt/ext/openexr/IlmBase/IlmThread/IlmThread.cpp \
+  /Users/alan/Src/Games202/A2/prt/build/ext_build/openexr/IlmBase/config/IlmBaseConfig.h
---
+++ A2/prt/build/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphore.cpp.o.d
@@ -0,0 +1,3 @@
+ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/IlmThreadSemaphore.cpp.o: \
+  /Users/alan/Src/Games202/A2/prt/ext/openexr/IlmBase/IlmThread/IlmThreadSemaphore.cpp \
+  /Users/alan/Src/Games202/A2/prt/build/ext_build/openexr/IlmBase/config/IlmBaseConfig.h
